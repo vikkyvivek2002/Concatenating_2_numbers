9aa0d6d84579b7c417ae7afd38f7b5228053b34e
vikkyvivek2002 <84318895+vikkyvivek2002@users.noreply.github.com>
2021-06-19 23:38:04 +0530

    Add files via upload

---
+++ Concatenating_2_numbers.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Concatenating_2_numbers
+{
+public static void main(String[] args)
+{
+Scanner s = new scanner(System.in);
+System.out.println("Enter two numbers :");
+int a = s.nextInt();
+int b = s.nextInt();
+String s1 = Integer.toString(a);
+string s2 = Integer.toString(b);
+String s3 = s1 + s2;
+int c = Integer.parseInt(s3);
+system.out.println(c);
+}
+}
